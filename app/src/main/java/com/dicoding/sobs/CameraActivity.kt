965d42207112482659634c724a9c4414198a5730
Hanif <hanifsatrio12@gmail.com>
2022-06-11 23:13:09 +0700

    add func button and fix recycler view

---
+++ app/src/main/java/com/dicoding/sobs/CameraActivity.kt
@@ -0,0 +1,56 @@
+package com.dicoding.sobs
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.dicoding.sobs.databinding.ActivityCameraBinding
+
+class CameraActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCameraBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.captureImage.setOnClickListener { takePhoto() }
+        binding.switchCamera.setOnClickListener { startCamera() }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+        startCamera()
+    }
+
+    private fun takePhoto() {
+        // takePhoto
+    }
+
+    private fun startCamera() {
+        // showCamera
+    }
+
+    private fun hideSystemUI() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+}
--- app/src/main/java/com/dicoding/sobs/MainActivity.kt
+++ app/src/main/java/com/dicoding/sobs/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.dicoding.sobs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -10,10 +12,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btn_setor : CardView = findViewById(R.id.btn_setor)
         val recyclerView : RecyclerView = findViewById(R.id.rv_sampah)
         val adapter = SampahAdapter(fetchData())
         recyclerView.layoutManager = GridLayoutManager(this,2)
         recyclerView.adapter = adapter
+        btn_setor.setOnClickListener(){
+            val setorIntent = Intent(this,
+                SetorActivity::class.java)
+            startActivity(setorIntent)
+        }
     }
 
     private fun fetchData() : ArrayList<String> {
